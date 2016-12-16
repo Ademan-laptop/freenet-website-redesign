0417bb3e9b1732721c328c91efd917e3af9de11b
Daniel Roberts <Ademan555@gmail.com>
2016-12-15 19:28:37 -0800

    fixup plugin

--- _plugins/freenet_i18n.rb
+++ _plugins/freenet_i18n.rb
@@ -9,11 +9,6 @@ def strip_heredoc(s)
 end
 
 class GetTextTag < Liquid::Block
-	#include Liquid::StandardFilters
-
-	#def initialize(tag_name, markup, tokens)
-	#end
-	
 	def get_locale(context)
 		page = context.registers[:page]
 
@@ -22,23 +17,6 @@ class GetTextTag < Liquid::Block
 
 	# FIXME there must be a better way to initialize global state
 	def get_translation(context)
-		#if context.registers[:translations] == nil
-			#context.registers[:translations] = {}
-		#end
-
-		#locale = self.get_locale(context)
-
-		#if context.registers[:translations][locale] != nil
-			#return context.registers[:translations][locale]
-		#end
-
-		#site = context.registers[:site]
-		#puts "path: #{path}"
-		#puts "Page count: ", pages.length
-		#page = self.get_page(context)
-		#return page['locale']
-		#return pages.first.locale
-
 		page = context.registers[:page]
 		path = page['path']
 		pages = self.get_pages(context)
@@ -88,69 +66,17 @@ class TranslatedPage < Jekyll::Page
 		@locale_name = locale_name
 		@locale = locale
 
-		#puts "name: #{name} dir: #{dir}"
-		super(site, base, dir, name) #localized_name)
+		super(site, base, dir, name)
 		@name = name
 	end
 
-	#def path
-	#end
-
-	def template
-		if !html?
-			#puts "/:path/:basename_:locale:output_ext"
-			"/:path/:basename_:locale:output_ext"
-		elsif index?
-			#puts "/:path_:locale/"
-			"/:path_:locale/"
-		else
-			Jekyll::Utils.add_permalink_suffix("/:path/:basename.:locale", site.permalink_style)
-		end
-	end
-
-	#def url_placeholders
-		#{
-			#:path       => @dir,
-			#:basename   => basename + "." + @locale_name,
-			#:output_ext => output_ext,
-			#:locale     => @locale_name
-		#}
-	#end
-
 	def url
-		#puts "trace url()"
-		#puts self.url_placeholders()
-		#@url ||= Jekyll::URL.new({
-			#:template     => template,
-			#:placeholders => self.url_placeholders(),
-			#:permalink    => permalink
-		#}).to_s
-
-		#puts "Path"
-		#puts path
-		#puts template
-		#puts @url
-		#return @url
-		#
-			#:path       => @dir,
-			#:basename   => basename + "." + @locale_name,
-			#:output_ext => output_ext,
-			#:locale     => @locale_name
-		#puts @dir
-		#puts "path: " + url
-
-		
-
 		url = "/" + basename + "." + @locale_name + "/"
 		if @dir != "."
 			url = "/" + @dir + url
 		end
 		return url
 	end
-	# TODO override url
-	#def path
-		#return name
-	#end
 end
 
 class GetTextGenerator < Jekyll::Generator
@@ -186,22 +112,6 @@ class GetTextGenerator < Jekyll::Generator
 
 				dir = File.dirname(page.path)
 				new_pages << TranslatedPage.new(site, site.source, dir, page.name, new_path, locale_name, locale)
-
-				#puts page.name
-				#if extension != 'md'
-					#continue # FIXME: no continue in ruby?
-				#end
-				#puts new_path
-
-				#new_pages << TranslatedPage.new(site, page.base, page.dir, new_path)
-				#page.base
-				#page.dir
-				#puts "page.dir: ", page.dir
-				#puts "page.path: ", page.path
-				# FIXME: I don't think page.dir is correct below
-				# FIXME: it is not correct, it is the url dir not the source dir... need to figure this out
-				# NOTE: Not certain if dir is correct either, at least suits our purpose
-				#puts "url: #{new_pages.last.url}"
 			end
 		end
 
